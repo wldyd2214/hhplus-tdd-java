23633b71ebff40a14e54c39205de0674f293da60
wldyd2214 <wldyd2214@naver.com>
2024-06-17 00:45:34 +0900

    Feat: controller 패키지 생성, 공통 ApiResponse 추가

--- src/main/java/io/hhplus/tdd/api/controller/point/PointController.java
+++ src/main/java/io/hhplus/tdd/api/controller/point/PointController.java
@@ -1,5 +1,8 @@
-package io.hhplus.tdd.point;
+package io.hhplus.tdd.api.controller.point;
 
+import io.hhplus.tdd.api.ApiResponse;
+import io.hhplus.tdd.point.PointHistory;
+import io.hhplus.tdd.point.UserPoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -36,11 +39,12 @@ public class PointController {
      * TODO - 특정 유저의 포인트를 충전하는 기능을 작성해주세요.
      */
     @PatchMapping("{id}/charge")
-    public UserPoint charge(
-            @PathVariable long id,
+    public ApiResponse<UserPoint> charge(
+            @PathVariable("id") long id,
             @RequestBody long amount
     ) {
-        return new UserPoint(0, 0, 0);
+        //return new UserPoint(0, 0, 0);
+        return ApiResponse.ok(new UserPoint(0, 0, 0));
     }
 
     /**
