5bab023ac7ad0e2bb8bf569852ed60906dfcfcd9
wldyd2214 <wldyd2214@naver.com>
2024-06-19 00:00:57 +0900

    Feat: PointRepositoryTest 수정

--- src/test/java/io/hhplus/tdd/api/point/PointRepositoryTest.java
+++ src/test/java/io/hhplus/tdd/api/point/PointRepositoryTest.java
@@ -8,29 +8,26 @@ import io.hhplus.tdd.point.PointRepositoryImpl;
 import io.hhplus.tdd.point.UserPoint;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-//@Transactional
+@SpringBootTest
 class PointRepositoryTest {
 
-    private final PointRepository PointRepository;
+    @Autowired
+    private PointRepository PointRepository;
 
-    public PointRepositoryTest(PointRepositoryImpl pointRepositoryImpl) {
-        this.PointRepository = pointRepositoryImpl;
-    }
-
-    @DisplayName("특정 유저의 포인트를 충전한다.")
+    @DisplayName("특정 유저의 포인트를 저장한다.")
     @Test
     void insertOrUpdate() {
         long id = 1L;
-        long amount = 100L;
+        long amount = 1000L;
 
         // when
         UserPoint userPoint = PointRepository.insertOrUpdate(id, amount);
 
         // then
         assertThat(userPoint).extracting("id", "point")
-                .containsExactlyInAnyOrder(
-                        tuple(1L, 100L)
-                );
+                             .contains(id, amount);
     }
 }
