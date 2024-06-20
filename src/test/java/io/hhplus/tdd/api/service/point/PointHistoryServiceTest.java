31ca503b73bc3acee57d4842dea73b62d7f48fda
wldyd2214 <39686405+wldyd2214@users.noreply.github.com>
2024-06-20 20:24:01 +0900

    Refector: 접근제어자 변경

--- src/test/java/io/hhplus/tdd/api/service/point/PointHistoryServiceTest.java
+++ src/test/java/io/hhplus/tdd/api/service/point/PointHistoryServiceTest.java
@@ -15,7 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class PointHistoryServiceTest {
+class PointHistoryServiceTest {
 
     @Autowired
     private PointHistoryRepository pointHistoryRepository;
