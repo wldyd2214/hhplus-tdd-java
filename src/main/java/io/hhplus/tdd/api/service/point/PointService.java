baf033a0861502ca2d68932c1394b9f48dedbedf
wldyd2214 <39686405+wldyd2214@users.noreply.github.com>
2024-06-21 19:59:55 +0900

    Feat: 포인트 충전/사용시 synchronized 키워드 추가
    synchronized 키워드 추가하여 현제 데이터를 사용하고 있는 해당 스레드를 제외하고 나머지 스레드들은 테이터에 접근 할 수 없도록 처리

--- src/main/java/io/hhplus/tdd/api/service/point/PointService.java
+++ src/main/java/io/hhplus/tdd/api/service/point/PointService.java
@@ -11,6 +11,8 @@ import io.hhplus.tdd.point.UserPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 @RequiredArgsConstructor
 @Service
 public class PointService {
@@ -31,21 +33,23 @@ public class PointService {
         return userPointUsedProcess(id, amount);
     }
 
-    private UserPoint chargeUserPointProcess(long id, long amount) {
-        UserPoint userPoint = insertUserPointOrUpdate(id, amount);
+    private synchronized UserPoint chargeUserPointProcess(long id, long amount) {
+        UserPoint userPoint = selectUserPointById(id);
+        long sumPoint = userPoint.point() + amount;
         insertPointChargeHistory(id, amount);
-        return userPoint;
+        System.out.println(String.format("[chargeUserPointProcess] - %d번 유저의 포인트는 총 %d 입니다. ", id, sumPoint));
+        return insertUserPointOrUpdate(id, sumPoint);
     }
 
-    private UserPoint userPointUsedProcess(long id, long amount) {
+    private synchronized UserPoint userPointUsedProcess(long id, long amount) {
         UserPoint userPoint = selectUserPointById(id);
+        System.out.println(String.format("[userPointUsedProcess] - %d번 유저의 %d 포인트가 존재하고 %d 만큼 사용하고 싶어요.", id, userPoint.point(), amount));
         if (userPoint.point() < amount)
             throw new IllegalArgumentException("보유 포인트가 부족합니다.");
 
         long usedPoint = usedPointCalculator(userPoint.point(), amount);
 
         UserPoint resultUserPoint = insertUserPointOrUpdate(userPoint.id(), usedPoint);
-
         insertPointUseHistory(id, amount);
 
         return resultUserPoint;
